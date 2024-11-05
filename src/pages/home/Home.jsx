8dd4e50d18f6f755810a1a0efbfbeda8951bbc7e
DonPabla <zaderzedar@gmail.com>
2024-11-05 15:14:21 +0600

    Коммит от Ырыса

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
 import './Home.css';
+import { Link } from 'react-router-dom';
+
+const TaskCards = () => {
+    return (
+        <div className="tasks">
+            <Link to="#" className="priority-card">Priority Task <br /> 23/34</Link>
+            <Link to="#" className="upcoming-card">Upcoming Task <br /> 3/34</Link>
+            <Link to="#" className="overdue-card">Overdue Task <br /> 10/34</Link>
+            <Link to="#пше" className="pending-card">Pending Task <br /> 2/34</Link>
+        </div>
+    );
+};
 
 const Home = () => {
     return (
@@ -32,12 +44,9 @@ const Home = () => {
                     </div>
                 </div>
                 <div className="my-task">My Task</div>
-                <div className="tasks">
-                    <div className="priority-card">Priority Task <br /> 23/34</div>
-                    <div className="upcoming-card">Upcoming Task <br /> 3/34</div>
-                    <div className="overdue-card">Overdue Task <br /> 10/34</div>
-                    <div className="pending-card">Pending Task <br /> 2/34</div>
-                </div>
+
+                {}
+                <TaskCards />
 
                 <div className="urgency">
                     <h3>Urgently task</h3>
@@ -54,9 +63,9 @@ const Home = () => {
                 <div className="workspace">
                     <h3>Workspace</h3>
                     <div className="project-row">
-                        <div className="project-card">Dstudio Space</div>
-                        <div className="project-card">Team Space</div>
-                        <div className="project-card">CO Space</div>
+                        <Link to="#" className="project-card">Dstudio Space</Link>
+                        <Link to="#" className="project-card">Team Space</Link>
+                        <Link to="#" className="project-card">CO Space</Link>
                     </div>
                 </div>
             </div>
